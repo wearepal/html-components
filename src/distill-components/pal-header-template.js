515e5b7472fcd888ff96c5441dbbffe11f01d9ab
thomkeh <thomas5max@gmail.com>
2020-09-07 16:18:38 +0100

    Update URL in source code as well

--- src/distill-components/pal-header-template.js
+++ src/distill-components/pal-header-template.js
@@ -71,9 +71,9 @@ pal-header .nav a {
     Predictive Analytics Laboratory
   </a>
   <nav class="nav">
-    <a href="https://predictive-analytics-lab.github.io/reading">Reading Group</a>
-    <a href="https://predictive-analytics-lab.github.io/publications">Publications</a>
-    <a href="https://predictive-analytics-lab.github.io/jobs">Jobs</a>
+    <a href="https://predictive-analytics-lab.com/reading">Reading Group</a>
+    <a href="https://predictive-analytics-lab.com/publications">Publications</a>
+    <a href="https://predictive-analytics-lab.com/jobs">Jobs</a>
   </nav>
 </div>
 `;
